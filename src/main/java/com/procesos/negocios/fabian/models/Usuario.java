929a76925429e4ac43f8ee8c74dd0af4dab37b64
fabianllopezv <fllopezv@ufpso.edu.co>
2022-10-27 21:28:03 -0500

    implementacion de spring security y JWT

--- src/main/java/com/procesos/negocios/fabian/models/Usuario.java
+++ src/main/java/com/procesos/negocios/fabian/models/Usuario.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.util.Date;
 
 @Data
@@ -14,16 +16,26 @@ import java.util.Date;
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-    @Column(length = 100, nullable = false)
+    private Long id;
+    @Column (length = 100, nullable = false)
+    @NotBlank(message = "el nombre no puede estar en blanco")
     private String nombre;
-    @Column(length = 300, nullable = false)
+    @Column (length = 300,nullable = false)
+    @NotBlank(message = "los apellidos no puede estar en blanco")
     private String apellidos;
-    @Column(length = 20, nullable = false)
+    @NotBlank(message = "el documento no puede estar en blanco")
+    @Column (length = 20,nullable = false)
     private String documento;
-    @Column(length = 100)
+    @Column (length = 100)
     private String direccion;
     private Date fechaNacimiento;
-    @Column(length = 20)
+    @Column(length = 15)
     private String telefono;
+    @Column(unique = true, length = 100, nullable = false)
+    @NotBlank(message = "el correo no puede estar en blanco")
+    private String correo;
+    @Column(nullable = false, length = 64)
+    @NotBlank(message = "la contraseña no puede estar en blanco")
+    private String password;
+
 }
---
+++ src/main/java/com/procesos/negocios/fabian/services/UsuarioService.java
@@ -0,0 +1,17 @@
+package com.procesos.negocios.fabian.services;
+
+import com.procesos.negocios.fabian.models.Usuario;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface UsuarioService {
+    ResponseEntity<Usuario> getUserById(Long id);
+    ResponseEntity<Usuario> createUser(Usuario usuario);
+    ResponseEntity<List<Usuario>> allUsers();
+    ResponseEntity<List<Usuario>> allUsersByNameAndLastName(String nombre, String apellidos);
+    ResponseEntity<List<Usuario>> allUsersByName(String nombre);
+    ResponseEntity<List<Usuario>> allUsersByLastName( String apellidos);
+    ResponseEntity<Usuario> editUser(Long id, Usuario usuario);
+    ResponseEntity<Usuario> deleteUserById(Long id);
+}
